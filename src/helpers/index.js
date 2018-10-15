f96c042b2df890386ac5fda35d04ea6134f40169
Max Sylver <raranmariano@hotmail.com>
2018-10-15 16:32:52 -0300

    removing unnecessary download func

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,4 @@
 const greetings = ['Operator', 'Star-Child']
-// const http = require('http')
-
 
 module.exports = {
 
@@ -13,55 +11,3 @@ module.exports = {
     return string.charAt(0).toUpperCase() + string.slice(1)
   },
 }
-
-// function download(sub, func) {
-//   // http connection
-//   http.get(url + sub, (res) => {
-//     let body = ''
-
-//     // receiving data
-//     res.on('data', (chunk) => {
-//       body += chunk
-//     })
-
-//     // after the end of the stream
-//     res.on('end', () => {
-//       // calls function in the argument
-//       if (body !== '') {
-//         func(JSON.parse(body))
-//       }
-//       else {
-//         func('')
-//       }
-//     })
-
-//   // log an error
-//   }).on('error', (e) => {
-//     console.log('Got an error: ', e)
-//   })
-// }
-
-// function download(captalizedUrl, func) {
-//   // http connection
-//   http.get(captalizedUrl, (res) => {
-//     let body = ''
-
-//     // receiving data
-//     res.on('data', (chunk) => {
-//       body += chunk
-//     })
-
-//     // after the end of the stream
-//     res.on('end', () => {
-//       // calls function in the argument
-//       try {
-//         const jsonObj = JSON.parse(body)
-
-//         func(jsonObj)
-//       }
-//       catch (error) {
-//         console.log(error + body)
-//       }
-//     })
-//   })
-// }
