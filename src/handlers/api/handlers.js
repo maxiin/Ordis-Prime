48e5ec050d8c7f28b1e07c92deaaffcfe22e0daf
Max <raranmariano@hotmail.com>
2019-03-08 21:32:38 -0300

    simpler time return

--- src/handlers/api/handlers.js
+++ src/handlers/api/handlers.js
@@ -8,16 +8,14 @@ const NEWS_LIMIT = 6
 
 module.exports = {
   time: (response) => {
-    let finalStr = ''
-
     let dayNightCycle = response.earthCycle.isDay ? 'day' : 'night'
 
-    finalStr += `Game time: ${util.dateFormater(response.timestamp)}\n\n`
-    finalStr += `Earth' ${dayNightCycle} will end in ${response.earthCycle.timeLeft}\n` // Earth day will end in ...
-    finalStr += `Cetus: ${response.cetusCycle.shortString}\n` // cetus: 27 minutes to night
-    finalStr += `Vallis: ${response.vallisCycle.shortString}` // vallis 3 minutes to warm
-
-    return finalStr
+    return (
+      `Game time: ${util.dateFormater(response.timestamp)}\n\n` +
+      `Earth' ${dayNightCycle} will end in ${response.earthCycle.timeLeft}\n` +
+      `Cetus: ${response.cetusCycle.shortString}\n` +
+      `Vallis: ${response.vallisCycle.shortString}`
+    )
   },
 
   sortie(response) {
