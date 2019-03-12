db479e37652e1ee63c9a218bf6ddac04ef88f684
Max <raranmariano@hotmail.com>
2019-03-12 18:26:30 -0300

    fix not captalizating words that contained 'and'

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,7 +5,7 @@ module.exports = {
   greet: () => greetings[(Math.floor(Math.random() * greetings.length))],
 
   capitalize: (string) => {
-    if (string.includes('and')) {
+    if (string === 'and') {
       return string
     }
     return string.charAt(0).toUpperCase() + string.slice(1)
